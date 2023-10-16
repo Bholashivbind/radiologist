adee65fdf44e97b033b3569e07259eb19ac2706d
Shiv bhola <shivbhola17496@gmail.com>
2023-10-16 15:57:14 +0530

    src

---
+++ src/components/model_trainer.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
